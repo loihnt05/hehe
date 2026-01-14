f4d22a6e8d917c8db7fa5ccf315b8320235ba9be
loihnt05 <23520869@gm.uit.edu.vn>
2026-01-14 21:07:41 +0700

    new init

---
+++ MyWebApp/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Rewrite;
+using MyWebApp.Interfaces;
+using MyWebApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<IWelcomeService, WelcomeService>();
+
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"{context.Request.Method} {context.Request.Path} {context.Response.StatusCode}");
+    await next(); 
+});
+
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
+
+app.MapGet("/", async (IWelcomeService welcomeService1, IWelcomeService welcomeService2) => 
+{
+    string message1 = $"Message1: {welcomeService1.GetWelcomeMessage()}";
+    string message2 = $"Message2: {welcomeService2.GetWelcomeMessage()}";
+    return $"{message1}\n{message2}";
+});
+
+app.MapGet("/about", () => "Contoso was founded in 2000.");
+app.Run();
